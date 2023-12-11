152e70d336ee8f050079ac68d94fd5b8ff85746c
Nicole Heywood <nicsayhey@gmail.com>
2023-12-11 20:01:43 +0100

    a bunch of stuff on index, atm working on btn

--- js/script.js
+++ js/script.js
@@ -15,3 +15,76 @@ links.forEach((link) => {
   });
 });
 
+// smooth index-arrow scroll animation 
+
+document.addEventListener('DOMContentLoaded', function () {
+  const scrollLinks = document.querySelectorAll('.scroll-down-link');
+
+  scrollLinks.forEach(link => {
+    link.addEventListener('click', e => {
+      e.preventDefault();
+
+      const targetId = link.getAttribute('href');
+      const targetElement = document.querySelector(targetId);
+
+      if (targetElement) {
+        const offsetTop = targetElement.offsetTop;
+        const duration = 1000; // Change the duration as needed (in milliseconds)
+
+        smoothScroll(offsetTop, duration);
+      }
+    });
+  });
+
+  function smoothScroll(targetPosition, duration) {
+    const startingPosition = window.pageYOffset || document.documentElement.scrollTop;
+    const distance = targetPosition - startingPosition;
+    let startTime = null;
+
+    function animation(currentTime) {
+      if (startTime === null) {
+        startTime = currentTime;
+      }
+
+      const timeElapsed = currentTime - startTime;
+      const ease = easeOut(timeElapsed, startingPosition, distance, duration);
+      window.scrollTo(0, ease);
+
+      if (timeElapsed < duration) {
+        requestAnimationFrame(animation);
+      }
+    }
+
+    function easeOut(t, b, c, d) {
+      t /= d;
+      t--;
+      return c * (t * t * t + 1) + b;
+    }
+
+    requestAnimationFrame(animation);
+  }
+});
+
+// image animation
+document.addEventListener('DOMContentLoaded', function () {
+  const images = document.querySelectorAll('.image');
+
+  const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.3 // Adjust this threshold as needed
+  };
+
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('image-visible');
+        observer.unobserve(entry.target);
+      }
+    });
+  }, options);
+
+  images.forEach(image => {
+    observer.observe(image);
+  });
+});
